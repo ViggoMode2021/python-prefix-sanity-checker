f8319af360199603777358476465441b8d1bc946
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2025-02-25 20:44:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,31 @@ def prefix_list_checker():
         except ValueError:
             print(f"{subnet} is not a valid subnet with CIDR notation.")
 
-    greater_than_mask = int(input(f"ip prefix-list TEST permit {subnet} ge __ le __"))
+    while True:
 
-    less_than_mask = int(input(f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le __"))
+        greater_than_mask = (input(f"ip prefix-list TEST permit {subnet} ge __ le __"))
+
+        try:
+            greater_than_mask = int(greater_than_mask)
+            if 0 <= greater_than_mask <= 32:
+                break
+            elif greater_than_mask > 32:
+                print(f"Input {greater_than_mask} is out of valid range.")
+        except ValueError:
+            print(f"{greater_than_mask} is not a valid integer!")
+
+    while True:
+
+        less_than_mask = int(input(f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le __"))
+
+        try:
+            less_than_mask = int(less_than_mask)
+            if 0 <= less_than_mask <= 32:
+                break
+            elif less_than_mask > 32:
+                print(f"Input {less_than_mask} is out of valid range.")
+        except ValueError:
+            print(f"{less_than_mask} is not a valid integer!")
 
     full_statement = f"ip prefix-list TEST permit {subnet} ge {greater_than_mask} le {less_than_mask}"
 
@@ -27,27 +49,25 @@ def prefix_list_checker():
     while True:
         subnet_inputs = input("Add subnets that you would like to check.")
 
-        subnet_lists.append(subnet_inputs)
-
-        cidr_pattern = r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$'
-
         if subnet_inputs == "done":
-            subnet_lists.remove("done")
             break
-        elif not re.match(subnet_inputs, cidr_pattern):
-            print(f"{subnet_inputs} does not match regex for prefix + cidr")
 
-        subnet_lists.append(subnet_inputs)
+        try:
+            ipaddress.ip_network(subnet_inputs)
+            subnet_lists.append(subnet_inputs)
+        except ValueError:
+            print(f"{subnet_inputs} is not a valid subnet with CIDR notation.")
 
-    for network in subnet_lists:
-        mask = int(network.split("/", 1)[1])
+        for network in subnet_lists:
+            mask = int(network.split("/", 1)[1])
 
-        if IPNetwork(network) in IPNetwork(subnet) and greater_than_mask <= mask <= less_than_mask:
-            print(f"Yes, {network} is in {subnet} and meets the criteria of your prefix list!")
-        elif IPNetwork(network) in IPNetwork(subnet):
-            print(print(f"The {network} network is in {subnet}, but doesn't meet the comparison operator criteria."))
-        else:
-            print(f"No, {network} does not meet the criteria of your prefix list.")
+            if IPNetwork(network) in IPNetwork(subnet) and greater_than_mask <= mask <= less_than_mask:
+                print(f"Yes, {network} is in {subnet} and meets the criteria of your prefix list!")
+                subnet_lists.remove(subnet_inputs)
+            elif IPNetwork(network) in IPNetwork(subnet):
+                print(print(f"The {network} network is in {subnet}, but doesn't meet the comparison operator criteria."))
+            else:
+                print(f"No, {network} does not meet the criteria of your prefix list.")
 
 if __name__ == "__main__":
     prefix_list_checker()
